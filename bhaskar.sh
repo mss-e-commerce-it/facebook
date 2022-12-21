f7102b51206b92cb4314e721643d2a6cf4b035d8
raghunathreddy70 <raghunathreddy5751@gmail.com>
2022-12-21 12:42:55 +0530

    updated

--- bhaskar.sh
+++ bhaskar.sh
@@ -1,9 +1,4 @@
 this bhaskar file
-<<<<<<< HEAD
-hii bhaskar iam aryan
-=======
-this raghu edit file
->>>>>>> 905f751fb83c8f5965bbee1a006fd56b8a27b6c4
-
-up dat
 updated file
+iam using this file
+
